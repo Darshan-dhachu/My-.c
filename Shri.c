3f98dd9b2ada04da1f3c7bc4ffeeedd35c9fdeea
darshancoder <cldarshan16@gmail.com>
2025-05-13 15:42:56 +0530

    Create Shri.c

---
+++ Shri.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int shridevi(){
+    printf("name:darshan\n");
+    printf("ph:7760527360\n");
+    printf("DOB:2/11/2006\n");
+    printf("age=19\n");
+    
+}
+int main()
+{
+    shridevi();
+}
