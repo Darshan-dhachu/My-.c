a8613598ce369fb12b05c8081b6a1339f1034004
darshancoder <cldarshan16@gmail.com>
2025-05-13 21:45:41 +0530

    Create  area of circle.c

---
+++ area of circle.c
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int r;
+    float pi=3.14,area;
+    printf("enter the radius of a circle:");
+    scanf("%d",&r);
+    area=pi*r*r;
+    printf("area of a ciecle=%f",area);
+}
